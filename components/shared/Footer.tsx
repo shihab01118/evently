5375006485be9fad8523aa94b9d35f1ee5193e16
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-12 17:25:02 +0600

    footer added

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image
+            src='/assets/images/logo.svg'
+            alt='logo'
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>&copy; {new Date().getFullYear()} Evently. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
