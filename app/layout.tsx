7bbe96ed141df1b5f243a10999f5896e446e39db
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-12 17:11:50 +0600

    navbar added for both mobile and desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -9,8 +10,8 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Eventrix',
-  description: 'Eventrix - Your Event Management Solution',
+  title: 'Evently',
+  description: 'Evently - Your Event Management Solution',
   icons: {
     icon: '/assets/images/logo.svg'
   }
@@ -22,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
